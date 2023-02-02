69190b00c7114c6698a027aa6eee371469de383e
MiquelAngelMontero <gitmmontero@gmail.com>
2023-02-02 10:09:02 +0100

    test

---
+++ src/test/java/com/backend/AppTitu/AppTituApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.AppTitu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTituApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

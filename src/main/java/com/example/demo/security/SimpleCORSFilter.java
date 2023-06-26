65a438e4386f1db016542cfdddbf1c869924b577
MiquelAngelMontero <97508353+MiquelAngelMontero@users.noreply.github.com>
2023-06-26 12:06:59 +0200

    Update SimpleCORSFilter.java

--- src/main/java/com/example/demo/security/SimpleCORSFilter.java
+++ src/main/java/com/example/demo/security/SimpleCORSFilter.java
@@ -1,50 +1,42 @@
 package com.example.demo.security;
 
-import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+import java.io.IOException;
 
 @Component
-public class SimpleCORSFilter implements Filter{
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class SimpleCORSFilter implements Filter {
 
-	private final Logger log = LoggerFactory.getLogger(SimpleCORSFilter.class);
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // No se requiere ninguna inicialización adicional
+    }
 
-	public SimpleCORSFilter() {
-	    log.info("SimpleCORSFilter init");
-	}
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
 
-	@Override
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Max-Age", "3600");
 
-	    HttpServletRequest request = (HttpServletRequest) req;
-	    HttpServletResponse response = (HttpServletResponse) res;
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
 
-	    response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-	    response.setHeader("Access-Control-Allow-Credentials", "true");
-	    response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
-	    response.setHeader("Access-Control-Max-Age", "3600");
-	    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Host, Content-Lenght");
-
-	    chain.doFilter(req, res);
-	}
-
-	@Override
-	public void init(FilterConfig filterConfig) {
-		
-	}
-
-	@Override
-	public void destroy() {
-		
-	}
-
+    @Override
+    public void destroy() {
+        // No se requiere ninguna limpieza adicional
+    }
 }

4b17b7b47df662fb7b26329d2c9a3468706d7861
MiquelAngelMontero <97508353+MiquelAngelMontero@users.noreply.github.com>
2023-06-29 12:10:50 +0200

    Update SimpleCORSFilter.java

--- src/main/java/com/example/demo/security/SimpleCORSFilter.java
+++ src/main/java/com/example/demo/security/SimpleCORSFilter.java
@@ -11,9 +11,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.core.Ordered;
 
 @Component
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class SimpleCORSFilter implements Filter{
 
 	private final Logger log = LoggerFactory.getLogger(SimpleCORSFilter.class);
@@ -22,22 +26,23 @@ public class SimpleCORSFilter implements Filter{
 	    log.info("SimpleCORSFilter init");
 	}
 
-	@Override
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-	        throws IOException, ServletException {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
 
-	    HttpServletRequest request = (HttpServletRequest) req;
-	    HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+        response.setHeader("Access-Control-Max-Age", "3600");
 
-	    response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-	    response.setHeader("Access-Control-Allow-Credentials", "true");
-	    response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-	    response.setHeader("Access-Control-Max-Age", "3600");
-	    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Host, Content-Lenght, Authorization"); // Agrega 'Authorization' al encabezado
-
-	    chain.doFilter(req, res);
-	}
-
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
 	@Override
 	public void init(FilterConfig filterConfig) {
 		

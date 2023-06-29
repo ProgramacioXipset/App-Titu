2a6c2b5000af35a6607d9ea97dc1ab39fd42c1c9
MiquelAngelMontero <97508353+MiquelAngelMontero@users.noreply.github.com>
2023-06-29 13:29:47 +0200

    Update SimpleCORSFilter.java

--- src/main/java/com/example/demo/security/SimpleCORSFilter.java
+++ src/main/java/com/example/demo/security/SimpleCORSFilter.java
@@ -33,7 +33,7 @@ public class SimpleCORSFilter implements Filter{
         HttpServletRequest request = (HttpServletRequest) req;
 
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
         response.setHeader("Access-Control-Max-Age", "3600");
 
